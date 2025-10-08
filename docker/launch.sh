e887f10ccb47d6638ebfaaf5f6a88cd833f0dbd2
Michael Specht <micha.specht@gmail.com>
2025-10-08 20:22:43 +0200

    fixed rust config

--- docker/launch.sh
+++ docker/launch.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 rsync -a --safe-links --no-owner --no-group --omit-dir-times \
       --chmod=Du=rwx,Fu=rwX /src/ /app/
 
+source /home/runner/.cargo/env
+
 cd /app
 chmod +x start.sh
 exec ./start.sh
